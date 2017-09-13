01b1ac577f9cc985883594196734d49114115007
davidmgudeman <davidmgudeman@gmail.com>
2017-09-13 15:31:52 -0700

    requestOnTimePassword is working

--- components/signup.js
+++ components/signup.js
@@ -2,12 +2,19 @@
 import React, { Component } from 'react';
 import { View, Text } from  'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
 
+const ROOT_URL = 'https://us-central1-one-time-password-ea52b.cloudfunctions.net'
 class SignUpForm extends Component {
   state = { phone: '' };
 
-  handleSubmiti = () => {
-
+  handleSubmit = async () => {
+    try {
+      await axios.post( `${ROOT_URL}/createUser`, { phone: this.state.phone })
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone } )
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {

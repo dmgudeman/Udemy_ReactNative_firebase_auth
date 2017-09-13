9167f422e0c5c329df5bcf777c55245904087049
davidmgudeman <davidmgudeman@gmail.com>
2017-09-13 15:56:58 -0700

    implemented the SignInForm

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SignUpForm from './components/signup';
+import SignInForm from './components/SignIn';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
---
+++ components/SignIn.js
@@ -0,0 +1,43 @@
+
+import React, { Component } from 'react';
+import { View, Text } from  'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-ea52b.cloudfunctions.net'
+class SignInForm extends Component {
+  state = { phone: '', code: '' };
+
+  handleSubmit = async () => {
+    try {
+      await axios.post(`${ROOT_URL}/verifyOneTimePassword`, { phone: this.state.phone, code: this.state.code } )
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput 
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
+          <FormLabel>Enter Code</FormLabel>
+          <FormInput 
+            value={this.state.code}
+            onChangeText={code => this.setState({ code })}
+          />
+        </View>
+        <Button 
+          title="Submit"
+          onPress={this.handleSubmit} 
+          />
+      </View>
+    )
+  }
+}
+
+export default SignInForm;
